8b963486c9a09895b10b497c8672387678f3d389
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-09-18 16:56:22 +0100

    fixed hero width

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className='w-screen max-w-full h-screen bg-zinc-900 flex flex-col justify-between'>
-      <div className='grid md:grid-cols-2 max-w-7xl m-auto '>
-        <div className='flex flex-col md:items-start w-full px-2 py-8'>
+    <div className=' h-screen bg-zinc-900 flex flex-col justify-between'>
+      <div className='grid md:grid-cols-2 max-w-7xl m-auto'>
+        <div className='flex flex-col md:items-start px-2 py-8'>
           <p className='text-2xl text-accent'>Tarrot Reading, TikTok, portal</p>
-          <h1 className='py-3 text-5xl md:text-6xl font-bold text-primary font-Space '>HEARTBEAT SPIRIT COMMUNICATIONS</h1>
+          <h1 className='py-3 text-4xl md:text-6xl md:text-center font-bold text-primary '>HEARTBEAT SPIRIT COMMUNICATIONS</h1>
           <button className='btn btn-secondary text-neutral'>Book a Reading</button>
         </div>
       </div>
