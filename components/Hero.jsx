3a5155e26920af4340d801981c6558c5c744cc36
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-09-19 16:21:31 +0100

    added space font and hero img

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import planchette from '../public/images/planchette.png';
+import hero1 from '../public/images/hero1.png';
 import Image from 'next/image';
 
 const Hero = () => {
@@ -7,12 +7,16 @@ const Hero = () => {
     <div className=' h-screen bg-zinc-900 flex flex-col justify-between'>
       <div className='grid md:grid-cols-2 max-w-7xl m-auto'>
         <div className='flex flex-col md:items-start px-2 py-8 text-center md:text-left'>
-          <p className='text-2xl text-accent'>Tarrot Reading, TikTok, portal</p>
-          <h1 className='py-3 text-4xl md:text-6xl font-bold text-primary '>HEARTBEAT SPIRIT COMMUNICATIONS</h1>
+          <p className='text-2xl text-accent'>
+            Paranormal Investigators | Tarot Readings | Portal Sessions
+          </p>
+          <h1 className='py-3 text-4xl md:text-6xl font-bold text-primary font-Space '>
+            HEARTBEAT SPIRIT COMMUNICATIONS
+          </h1>
           <button className='btn btn-secondary text-neutral'>Book a Reading</button>
         </div>
-        <div className='w-full mt-12 md:mt-0'>
-          <Image src={planchette} alt='planchette' />
+        <div className='opacity-5'>
+          <Image alt='/' src={hero1} />
         </div>
       </div>
     </div>
